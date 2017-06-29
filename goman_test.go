a9d52d9cda5b0d73c2f0cae89544e1c6b099e246
christophberger <christophberger@users.noreply.github.com>
2017-06-29 17:16:33 +0200

    Use testdata/goman

--- goman_test.go
+++ goman_test.go
@@ -43,7 +43,7 @@ func Test_findLocalReadme(t *testing.T) {
 		//	wantFp     string
 		wantErr bool
 	}{
-		{"goman", args{"github.com/christophberger/goman"}, false},
+		{"./testdata/goman", args{"github.com/christophberger/goman"}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
